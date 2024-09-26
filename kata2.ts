30988fc988032e70ab802e45dd752983045b88b9
Idrisse ANGAMA <idrisseangama2a@gmail.com>
2024-09-26 09:24:44 +0200

    kata 2

---
+++ kata2.ts
@@ -0,0 +1,43 @@
+/**
+ * 1 - Écris une fonction sumOfEvenNumbers(numbers) qui prend en 
+ * entrée un tableau d'entiers numbers et retourne la somme de tous 
+ * les nombres pairs présents dans le tableau.
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// your code here
+const sumOfEvenNumbers = (numbers: number[]) : number  => {
+    const pairNumbers : number[] = numbers.filter(number => number%2 === 0);
+    //[2,4,6]
+    return pairNumbers.reduce( (acc: number, curr: number) => {
+        //premier tour acc = 0 curr = 2 acc devient 2
+        //second tour acc = 2 curr = 4 acc devient 6
+        //3e tour acc = 6 curr = 6 acc devient 12
+        /* total itérations : 3 */
+        return acc += curr;
+    },0);
+};
+
+console.log("La somme des nombres pairs = ",sumOfEvenNumbers(numbers));
+
+/**
+ * 2 - Écris une fonction maxNumber(numbers) qui prend en 
+ * entrée un tableau d'entiers numbers et retourne le plus grand 
+ * nombre présent dans le tableau.
+ */
+
+const numbers2 = [12, 21, 67, 36, 49, 51];
+
+
+// your code here
+const maxNumber = (numbers: number[]) : number => {
+    /*
+    spread op ...
+    si numbers = [1,2,3,4] permet d'éclater le tab et récup que les valeurs
+    ce qui donne => Math.max(1,2,3,4) => 4
+    */
+    return(Math.max(...numbers)); 
+};
+
+console.log("Le plus grand est : ",maxNumber(numbers2));
